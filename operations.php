360ed642b864633ea6a6082e50c8d745dedc390e
vishal1145 <vishal.kumar1145@gmail.com>
2019-02-13 17:41:15 +0530

    operation

--- operations.php
+++ operations.php
@@ -6,7 +6,7 @@ $con = $obj->connect();
 
 $password = $_POST['password'];
 	$id = $_POST['id'];
- $Time_Card = "UPDATE Employee SET password = md5('$password') WHERE empl_id = '$id'";
+ $Time_Card = "UPDATE employee SET password = md5('$password') WHERE empl_id = '$id'";
  $Time_Cardd = mysqli_query($con,$Time_Card);
 
  echo "password updated";
