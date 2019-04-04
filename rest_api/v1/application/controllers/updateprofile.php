cbef12391588e2a82f6f4f3c548bb7a58ffbbf31
vishal1145 <vishal.kumar1145@gmail.com>
2019-04-04 15:31:10 +0530

    ping api

--- rest_api/v1/application/controllers/updateprofile.php
+++ rest_api/v1/application/controllers/updateprofile.php
@@ -13,65 +13,12 @@ class updateprofile extends CI_Controller {
 		$this->load->model('Employee_project');
 	}
 
-	/**
-	 * Index Page for this controller.
-	 *
-	 * Maps to the following URL
-	 * 		http://example.com/index.php/welcome
-	 *	- or -
-	 * 		http://example.com/index.php/welcome/index
-	 *	- or -
-	 * Since this controller is set as the default controller in
-	 * config/routes.php, it's displayed at http://example.com/
-	 *
-	 * So any other public methods not prefixed with an underscore will
-	 * map to /index.php/welcome/<method_name>
-	 * @see https://codeigniter.com/user_guide/general/urls.html
-	 */
-	public function test()
+	public function ping()
 	{
-		$emp_id = $this->input->get_post('emp_id', TRUE);
-		$firstname= $this->input->get_post('task_id', TRUE);
-		$lastname= $this->input->get_post('task_name', TRUE);
-		$image= $this->input->get_post('task_name', TRUE);
-		$hourlyrate= $this->input->get_post('task_name', TRUE);
-		
-		$this->form_validation->set_rules('emp_id','emp_id','trim|required');
-		$this->form_validation->set_rules('firstname','firstname','trim|required');
-		$this->form_validation->set_rules('lastname','lastname','trim|required');
-		$this->form_validation->set_rules('image','image','trim|required');
-		$this->form_validation->set_rules('hourlyrate','hourlyrate','trim|required');
-
-
-		if($this->form_validation->run() == FALSE)
-		{
-			$error = strip_tags(validation_errors());
-			$result = array(
-
-					'status'	 => '0',
-					'statuscode' => '403',
-					'msg'		 => $error
-
-			);
-			echo (json_encode($result));
-		}else {
-
-        $arr = array();
-
-		header('Content-Type: application/json');
-		echo json_encode( $arr );
-		}
-    }
-    
-    public function get_all_task()
-	{
-	
-
-        $query = $this->db->query("select * from project_tasks");
-        $arr =  $query->result_array();
-		header('Content-Type: application/json');
-		echo json_encode( $arr );
+		echo "Ping";
 	}
+
+
 	public function updateprofile()
 	{
 		$emp_id = $this->input->get_post('emp_id', TRUE);
@@ -163,16 +110,4 @@ class updateprofile extends CI_Controller {
 
 		
 		}	
-
-
-
-	public function delete_task()
-	{
-		$task_name= $this->input->get_post('task_name', TRUE);
-
-		$query = $this->db->query("DELETE FROM project_tasks WHERE task_name='$task_name';");
-        $arr =  $query->result_array();
-		header('Content-Type: application/json');
-		echo json_encode( $arr );
-	}
 }
